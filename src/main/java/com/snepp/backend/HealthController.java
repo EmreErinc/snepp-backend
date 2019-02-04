1935d1e22fa6287997f05f8e0e70bf3f23491ac9
emre erinc <emre.erinc@iven.io>
2019-02-04 15:55:10 +0300

    sneppController methods moved to healthController

--- src/main/java/com/snepp/backend/HealthController.java
+++ src/main/java/com/snepp/backend/HealthController.java
@@ -1,13 +1,32 @@
 package com.snepp.backend;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.snepp.backend.v1.model.request.SneppRequest;
+import com.snepp.backend.v1.model.response.SingleSneppResponse;
+import com.snepp.backend.v1.service.SneppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HealthController {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String health(){
     return "healthy connection";
+  }
+
+  private SneppService sneppService;
+
+  @Autowired
+  public HealthController(SneppService sneppService) {
+    this.sneppService = sneppService;
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/")
+  public boolean add(@RequestBody SneppRequest sneppRequest){
+    return sneppService.save(sneppRequest);
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/{sneppId}")
+  public SingleSneppResponse snepp(@PathVariable String sneppId){
+    return sneppService.getSnepp(sneppId);
   }
 }
