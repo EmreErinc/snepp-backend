56c347fcca9dfd7daaee7c8655201291103a55fc
emre erinc <emre.erinc@iven.io>
2019-02-10 14:25:13 +0300

    cors fix

--- src/main/java/com/snepp/backend/BackendApplication.java
+++ src/main/java/com/snepp/backend/BackendApplication.java
@@ -25,7 +25,7 @@ public class BackendApplication extends SpringBootServletInitializer {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/").allowedOrigins("http://localhost:8000");
+        registry.addMapping("/*").allowedOrigins("http://localhost:8000").maxAge(3600);
       }
     };
   }
